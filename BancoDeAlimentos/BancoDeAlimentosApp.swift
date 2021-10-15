0250978d7a0bfa9f8bdfd1d0a065cccfed964404
David Josué Marcial Quero <A00828702@itesm.mx>
2021-10-14 17:43:10 -0700

    Initial Commit

---
+++ BancoDeAlimentos/BancoDeAlimentosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoDeAlimentosApp.swift
+//  BancoDeAlimentos
+//
+//  Created by David Josué Marcial Quero on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BancoDeAlimentosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
